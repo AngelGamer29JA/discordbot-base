38dcf831c8489d39674df2eed7ac940f400ebdd8
AngelGamer29JA <angelgamer29ja@gmail.com>
2021-02-19 00:33:33 -0600

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 module.exports = {
 	title: 'discordbot',
 	prefix: 'p!',
-	token: 'NzYzODI0MjU3OTI2Mjk5NjQ5.X39Uxg.wnemQMy6uDPGBpaYn4zYTH453AE',
-	ownerID: '737781809516904498',
+	token: '',
+	ownerID: '',
 	color: '#0984e3',
 	colorError: '#d63031',
 	version: '1.0'
